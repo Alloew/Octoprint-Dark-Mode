c1eb4ab0e4bf297871570e84c7b1312f1bf94d25
Quinn Cooperstein <quinn.flynn.coop@gmail.com>
2022-04-14 14:32:26 -0500

    self

--- octoprint_darkmodeui/__init__.py
+++ octoprint_darkmodeui/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class DarkModePlugin(octoprint.plugin.types.OctoPrintPlugin, octoprint.plugin.core.RestartNeedingPlugin):
-    def get_assets():
+    def get_assets(self):
         return dict(
             css = ["css/darkmode.css"],
         )
