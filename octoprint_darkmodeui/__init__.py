10884d7f962a8ebd4876dcdfe003d5f28d5f5973
Quinn Cooperstein <quinn.flynn.coop@gmail.com>
2022-04-14 14:15:28 -0500

    Update __init__.py

--- octoprint_darkmodeui/__init__.py
+++ octoprint_darkmodeui/__init__.py
@@ -2,8 +2,12 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 
-class DarkModePlugin():
-    pass
+class DarkModePlugin(octoprint.plugin.types.OctoPrintPlugin, octoprint.plugin.core.SortablePlugin):
+    def will_handle_ui(self, req):
+        pass
+    
+    def on_ui_render(self, now, request, render_kwargs):
+        pass
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_implementation__ = DarkModePlugin()
